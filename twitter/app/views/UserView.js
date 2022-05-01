ca98c817b1a7919000429958894adbf79c224ece
Esmeralda Santoyo <58286718+EsmeSantoyo@users.noreply.github.com>
2022-05-01 14:17:11 -0500

    Add: test para retornar error al obtener valores inválidos

--- twitter/app/views/UserView.js
+++ twitter/app/views/UserView.js
@@ -10,7 +10,15 @@ class UserView {
         if (payload === null){
             response = new UserView("payload no existe")
         } else {
-            response = UserService.create(1, "carlogilmar", "Carlo")
+            if (typeof payload.id != "number"){
+                response = new UserView("necesitan tener un valor valido")
+            } else if (typeof payload.name != "string"){
+                response = new UserView("necesitan tener un valor valido")
+            } else if (typeof payload.username != "string"){
+                response = new UserView("necesitan tener un valor valido")
+            } else {
+                response = UserService.create(1, "carlogilmar", "Carlo")
+            }
         }
         return response
     }
